8fb77481663429cb99b9acd0a6311987a8fc4788
Jordan Wahab <jordan_wahab@unc.edu>
2023-05-08 14:42:42 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,18 +1,22 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs/yargs';
-import moment from 'moment-timezone';
 
-const options = yargs(process.argv.slice(2))
- .usage("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE")
- .help("h")
- .option("n", { alias: "north", describe: "Latitude: N positive", type: "string" })
- .option("s", { alias: "south", describe: "Latitude: S negative", type: "string" })
- .option("e", { alias: "east", describe: "Longitude: E positive", type: "string" })
- .option("w", { alias: "west", describe: "Longitude: W negative", type: "string" })
- .option("z", { alias: "zone", describe: "Time zone", type: "string" })
- .option("d", { alias: "day", describe: "Day to retrieve weather", type: "string" })
- .option("j", { alias: "json", describe: "Echo pretty JSON and exit", type: "string" })
- .argv;
+import minimist from 'minimist';
+import moment from 'moment-timezone';
+import fetch from "node-fetch";
+
+// Command line options
+var options = minimist(process.argv.slice(2));
+
+// Help option
+if (options.h) {
+ console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+ console.log("    -h            Show this help message and exit.");
+ console.log("    -n, -s        Latitude: N positive; S negative.");
+ console.log("    -e, -w        Longitude: E positive; W negative.");
+ console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+ console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+ console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+}
 
 const timezone = moment.tz.guess();
