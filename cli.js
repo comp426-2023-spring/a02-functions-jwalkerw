5c04b0a8c64c3987167b9297e13527bdd1d1ba31
Jordan Wahab <jordan_wahab@unc.edu>
2023-05-08 14:44:50 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -17,6 +17,7 @@ if (options.h) {
  console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
  console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
  console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+ process.exit(0);
 }
 
 const timezone = moment.tz.guess();
