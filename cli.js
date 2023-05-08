1d93f376d6527ec511c0f44643c0e2022afb94a7
Jordan Wahab <jordan_wahab@unc.edu>
2023-05-08 13:58:13 -0400

    Update cli.js

--- cli.js
+++ cli.js
@@ -14,11 +14,12 @@ const options = yargs(process.argv.slice(2))
  .argv;
 
 if (options.h) {
-  console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
-  console.log("    -h            Show this help message and exit.");
-  console.log("    -n, -s        Latitude: N positive; S negative.");
-  console.log("    -e, -w        Longitude: E positive; W negative.");
-  console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
-  console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
-  console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+ console.log("Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE");
+ console.log("    -h            Show this help message and exit.");
+ console.log("    -n, -s        Latitude: N positive; S negative.");
+ console.log("    -e, -w        Longitude: E positive; W negative.");
+ console.log("    -z            Time zone: uses tz.guess() from moment-timezone by default.");
+ console.log("    -d 0-6        Day to retrieve weather: 0 is today; defaults to 1.");
+ console.log("    -j            Echo pretty JSON from open-meteo API and exit.");
+ exit 0;
 }
